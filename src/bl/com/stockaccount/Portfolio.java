9310106a92b99c065e5018718e68d23593f2e810
Mr-Raja12 <rk9576899082@gmail.com>
2022-12-06 02:48:54 +0530

    Added: Writen a program to read in Stock Names, Number of Share, Share Price.

---
+++ src/bl/com/stockaccount/Portfolio.java
@@ -0,0 +1,22 @@
+package bl.com.stockaccount;
+import java.util.Scanner;
+/**
+ *
+ * @author Raja
+ *
+ */
+public class Portfolio extends StockAccount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the total number of Stocks");
+        setNoOfStocks(scanner.nextInt());
+        for (int i = 1; i <= getNoOfStocks(); i++) {     //initialize
+            StockAccount stock = new StockAccount();
+            stock.addShare();
+            TotalValue += StockPrice;
+        }
+        System.out.println("\nTotal Value is " + TotalValue);  //printed total value
+    }
+
+}
+
